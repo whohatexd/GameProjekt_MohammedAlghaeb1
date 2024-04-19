ace9b66ed51c42864d7e330127d76549851c45df
Mohammad Alghaeb <mohalg0620@staden.gotheborg.net>
2024-04-19 08:07:52 +0200

    Start Lektion 19/4

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hello
     }
 
     // Update is called once per frame
