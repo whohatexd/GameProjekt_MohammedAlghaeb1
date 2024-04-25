086a8a0a120541da4af1e8426daebd341d78af3b
Mohammad Alghaeb <mohalg0620@staden.gotheborg.net>
2024-04-25 12:13:52 +0200

    slut

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public GameObject CameraYspeed;
-    public float offset;
-    public float offsetSmooth;
+   
     private Vector3 playerpostion;
-    public GameObject a;
+    public GameObject player;
+    LoadGame Gotoscene;
    
      
 
     // Start is called before the first frame update
     void Start()
     {
-        playerpostion = a.transform.position;
+        playerpostion = player.transform.position;
+        Gotoscene = GetComponentInChildren<LoadGame>();
+        
     }
 
     // Update is called once per frame
@@ -23,9 +24,14 @@ public class CameraScript : MonoBehaviour
     {
         playerpostion = new Vector2(transform.position.y, transform.position.x);
 
-        if(transform.position.y < a.transform.position.y)
+        if(transform.position.y < player.transform.position.y)
         {
-            transform.position = new Vector3(transform.position.x, a.transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y > player.transform.position.y)
+        {
+            Gotoscene.GOtoScene("Main Menu");
         }
 
     }
